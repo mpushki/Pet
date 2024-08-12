70ec81fb88b2e045817bb0d571ebbed177c49eaf
maria pushkina <mariia.pushkina@gmail.com>
2024-08-12 17:46:31 +0300

    Added vue files

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,20 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue';
+import RegisterView from '@/views/RegisterView.vue';
+import LoginView from '@/views/LoginView.vue';
+import DashboardView from '@/views/DashboardView.vue';
+import ProfileView from '@/views/ProfileView.vue';
+import NoteView from '@/views/NoteView.vue';
+
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: "Home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/register',
+    name: 'Register',
+    component: RegisterView,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfileView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/note/:id',
+    name: 'Note',
+    component: NoteView,
+    meta: { requiresAuth: true },
+    props: true,
+  },
 ]
 
 const router = createRouter({
